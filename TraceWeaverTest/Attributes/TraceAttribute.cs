33b951a13a23926db61176163f141bd6701108d2
Nick Thijssen <lamah83@gmail.com>
2025-06-07 18:17:09 +0200

    Improved Handling of Non-Async Methods

--- TraceWeaverTest/Attributes/TraceAttribute.cs
+++ TraceWeaverTest/Attributes/TraceAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using MethodDecorator.Fody.Interfaces;
 using Microsoft.AspNetCore.Components;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace TraceWeaverTest.Attributes;
@@ -10,11 +11,12 @@ public class TraceAttribute : Attribute, IMethodDecorator
 {
 	private ComponentBase _targetInstance = default!;
 	private MethodBase _method = default!;
-
+	private bool _isAsync = false;
 	public void Init(object instance, MethodBase method, object[] args)
 	{
 		_targetInstance = (instance as ComponentBase)!;
 		_method = method;
+		_isAsync = method.IsDefined(typeof(AsyncStateMachineAttribute), false);
 	}
 
 	public void OnEntry()
@@ -27,10 +29,13 @@ public class TraceAttribute : Attribute, IMethodDecorator
 
 	public void OnExit()
 	{
-		 // do nothing
+		if (!_isAsync && DirtyHack.TracerService is not null)
+		{
+			DirtyHack.TracerService?.OnExit(_targetInstance, methodName: _method.Name);
+		}
 	}
 
-	public async Task OnTaskContinuation(Task task) 
+	public async Task OnTaskContinuation(Task task)
 	{
 		await task; // Ensure the task completes before proceeding
 		if (DirtyHack.TracerService is not null)
